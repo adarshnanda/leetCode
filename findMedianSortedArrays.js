f6f6ba33f3126257864f7d48bd9ef77751b3a226
adarshnanda <adarsh.ch.nanda@gmail.com>
2021-06-14 17:51:03 +0530

    Update findMedianSortedArrays.js

--- findMedianSortedArrays.js
+++ findMedianSortedArrays.js
@@ -4,9 +4,16 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    const mergedArray = []
+    let mergedArray = []
     const totalLen = nums1.length+nums2.length;
-    for(let i=0;i<totalLen;i++){
+    const num1leng = nums1.length-1;
+    const num2leng = nums2.length-1;
+    if(nums1[0]>=nums2[num2leng]){
+        mergedArray = nums2.concat(nums1);
+    }else if(nums2[0]>=nums1[num1leng]){
+        mergedArray = nums1.concat(nums2);
+    }else{
+        for(let i=0;i<totalLen;i++){
         const j = nums1.length-1;
         const k = nums2.length-1;
         if(j<0 || nums1[j]<nums2[k]){
@@ -14,7 +21,9 @@ var findMedianSortedArrays = function(nums1, nums2) {
         }else{
             mergedArray.push(nums1.pop());
         }
+        }
     }
+    
     const length = mergedArray.length;
     const mid = length/2;
     if(length%2){
